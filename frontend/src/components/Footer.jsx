3b6dc39ead7e6b1f9cec38d85e2c16e2902ebac3
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-07-07 01:30:14 +0200

    02. Create the Header and Footer Components.

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+import { Container, Row, Col } from 'react-bootstrap';
+
+const Footer = () => {  
+  const currentYear = new Date().getFullYear();
+  return (
+    <div>
+      <footer>
+        <Container>
+          <Row>
+            <Col className='text-center py3'>
+              <p>ProShop &copy; {currentYear}</p>
+            </Col>
+          </Row>
+        </Container>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
