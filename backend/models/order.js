884e533a1ee238ed30d8da298fa07e502b4a1c5d
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-07-07 22:24:01 +0200

    17. Create a Order model.

---
+++ backend/models/order.js
@@ -0,0 +1,87 @@
+const mongoose = require('mongoose');
+const { type } = require('os');
+
+const orderItems = new mongoose.Schema({
+  name: { type: String, required: true },
+  qty: { type: Number, required: true },
+  image: { type: String, required: true },
+  price: { type: Number, required: true },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'Product',
+  },
+});
+
+const shippingAddressSchema = new mongoose.Schema({
+  address: { type: String, required: true },
+  city: { type: String, required: true },
+  postalCode: { type: String, required: true },
+  country: { type: String, required: true },
+});
+
+const paymentMethodSchema = new mongoose.Schema({
+  id: { type: String },
+  status: { type: String },
+  update_time: { type: String },
+  email_address: { type: String },
+});
+
+const orderSchem = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: 'User',
+    },
+    orderItems: [orderItems],
+    shippingAddress: shippingAddressSchema,
+    paymentMethod: {
+      type: String,
+      required: true,
+    },
+    paymentMethod: paymentMethodSchema,
+    itemsPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    taxPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    shippingPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    totalPrice: {
+      type: Number,
+      required: true,
+      default: 0.0,
+    },
+    isPaid: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    paidAt: {
+      type: Date,
+    },
+    isDelivered: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+    deliveredAt: {
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = mongoose.model('Order', orderSchem);
+module.exports = Order;
