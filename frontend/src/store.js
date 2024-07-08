071d3289bb7cd0c6ef09f9f3e9b0fbd91e84c0b4
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-07-08 05:32:17 +0200

    24. Create a API clice in Redux.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { apiClice } from './slices/api';
 
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    [apiClice.reducerPath]: apiClice.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiClice.middleware),
+  devTools: true,
 });
 
 export default store;
