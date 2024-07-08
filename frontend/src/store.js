caf10b8d091210436ad0cf2fee43d236e910c368
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-07-08 17:51:34 +0200

    28. Create Reduc Slice for the Cart.

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiClice } from './slices/api';
+import cartSliceReducer from './slices/cart';
 
 const store = configureStore({
   reducer: {
     [apiClice.reducerPath]: apiClice.reducer,
+    cart: cartSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiClice.middleware),
