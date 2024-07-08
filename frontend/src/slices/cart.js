d86ee1106a43b35c356be2eb55cadcb68064f0fd
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-07-08 18:45:20 +0200

    29. Create a addToCard Reducer function for the Cart.

--- frontend/src/slices/cart.js
+++ frontend/src/slices/cart.js
@@ -4,10 +4,51 @@ const initialState = localStorage.getItem('cart')
   ? JSON.parse(localStorage.getItem('cart'))
   : { cartItems: [] };
 
+const addDecimals = (num) => (Math.round(num * 100) / 100).toFixed(2);
+
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
-  reducers: {},
-});
+  reducers: {
+    addToCart: (state, action) => {
+      const item = action.payload;
 
+      // Check if item exists
+      const itemExists = state.cartItems.find((i) => i._id === item._id);
+
+      // Update/Add item
+      if (itemExists) {
+        state.cartItems = state.cratItems.map((i) =>
+          i._id === item._id ? item : i
+        );
+      } else {
+        state.cartItems = [...state.cartItems, item];
+      }
+
+      // All items price
+      state.itemsPrice = addDecimals(
+        state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+      );
+
+      // Shipping price
+      state.shipoingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+
+      // Tax price
+      state.taxPrice = addDecimals(
+        Number((state.itemsPrice * 0.15).toFixed(2))
+      );
+
+      // Total price
+      state.totalPrice = (
+        Number(state.itemsPrice) +
+        Number(state.shipoingPrice) +
+        Number(state.taxPrice)
+      ).toFixed(2);
+
+      // Save to local storage
+      localStorage.setItem('cart', JSON.stringify(state));
+    },
+  },
+});
+export const { addToCart } = cartSlice.actions;
 export default cartSlice.reducer;
