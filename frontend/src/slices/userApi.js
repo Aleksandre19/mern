7eeaecc461adc033f78dd826a93dd3593a9e2242
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-07-09 04:45:21 +0200

    43. Frontend -> Create Login and Auth slices.

---
+++ frontend/src/slices/userApi.js
@@ -0,0 +1,18 @@
+import { USERS_URL } from '../constants';
+import { apiClice } from './api';
+
+export const userApi = () =>
+  apiClice.injectEndpoints({
+    endpoints: (builder) => ({
+      login: builder.mutation({
+        query: (data) => ({
+          url: USERS_URL / auth,
+          method: 'POST',
+          body: data,
+        }),
+        keepUnusedDataFor: 5,
+      }),
+    }),
+  });
+
+export const { useLoginMutation } = userApi();
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiClice } from './slices/api';
 import cartSliceReducer from './slices/cart';
+import authSliceReducer from './slices/auth';
 
 const store = configureStore({
   reducer: {
     [apiClice.reducerPath]: apiClice.reducer,
     cart: cartSliceReducer,
+    auth: authSliceReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiClice.middleware),
