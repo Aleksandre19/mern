f50baeae08924f5ed470edfe3d46b84d8a817acd
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-07-10 02:34:48 +0200

    57. Refactor Users Route.

--- backend/main.js
+++ backend/main.js
@@ -6,8 +6,9 @@ require('./startup/db.js')();
 require('./startup/logger')();
 
 const app = express();
-const products = require('./routes/products.js');
+const products = require('./routes/products');
 const users = require('./routes/users.js');
+const orders = require('./routes/orders');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -15,6 +16,7 @@ app.use(cookieParser());
 
 app.use('/api/products', products);
 app.use('/api/users', users);
+app.use('/api/orders', orders);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,27 +1,73 @@
 const express = require('express');
 const router = express.Router();
-const {
-  auth,
-  register,
-  logout,
-  getProfile,
-  updateProfile,
-  allUsers,
-  userById,
-  deleteUser,
-  updateUser,
-} = require('../controllers/user');
-const validateObjectId = require('../middlewares/validateObjectId');
-const { isAuth, isAdmin } = require('../middlewares/auth');
+const bcryptjs = require('bcryptjs');
+const _ = require('lodash');
+const User = require('../models/user');
+const { setAuthCookie } = require('../utils/auth');
+const asyncHandler = require('../middlewares/asyncHandler');
 
-router.route('/').post(register).get(isAuth, isAdmin, allUsers);
-router.post('/logout', logout);
-router.post('/auth', auth);
-router.route('/profile').get(isAuth, getProfile).put(isAuth, updateProfile);
-router
-  .route('/:id')
-  .get(isAuth, isAdmin, validateObjectId, userById)
-  .delete(isAuth, isAdmin, validateObjectId, deleteUser)
-  .put(isAuth, isAdmin, validateObjectId, updateUser);
+// @desc Authe User
+// @route POST /api/users/auth
+// @access Public
+router.post(
+  '/auth',
+  asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+
+    // Needs to be done Joi validation
+    if (!email || !password) return res.status(400).json('Invalid email or password');
+
+    // Validate email
+    const user = await User.findOne({ email });
+    if (!user) return res.status(401).json('Invalid email or password');
+
+    // Validate passwor
+    const validPassword = await bcryptjs.compare(password, user.password);
+    if (!validPassword) return res.status(400).json('Invalide email or password.');
+
+    // Generate Token and store it in HTTP-Only cookie
+    const token = user.generateAuthToken();
+    setAuthCookie(res, token);
+
+    // Return user
+    res.send(_.pick(user, ['_id', 'name', 'email', 'isAdmin']));
+  })
+);
+
+// @desc Register User
+// @route GET /api/users
+// @access Public
+router.post(
+  '/',
+  asyncHandler(async (req, res) => {
+    // !!! Needs to be add Joi validation
+
+    // Check if user exists
+    let user = await User.findOne({ email: req.body.email });
+    if (user) return res.status(400).json('User already exists');
+
+    // Create user
+    user = new User(_.pick(req.body, ['name', 'email', 'password', 'isAdmin']));
+    await user.save();
+
+    // Generate Token and store it in HTTP-Only cookie
+    const token = user.generateAuthToken();
+    setAuthCookie(res, token);
+
+    res.send(_.pick(user, ['_id', 'name', 'email', 'isAdmin']));
+  })
+);
+
+// @desc Logout User / Clear cookie
+// @route POST /api/users/logout
+// @access Private
+router.post(
+  '/logout',
+  asyncHandler(async (req, res) => {
+    res.clearCookie('mern_jwt');
+
+    res.status(200).json('User is logged out');
+  })
+);
 
 module.exports = router;
