12d2b9d28b08e21becff9585eace8c630f9c4874
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-07-13 01:58:00 +0200

    93. Set up a custom error middleware.

--- backend/main.js
+++ backend/main.js
@@ -10,11 +10,17 @@ db();
 import logger from './startup/logger.js';
 logger();
 
+import { notFound, errorHandler } from './middlewares/errorMiddleware.js';
+
 const app = express();
 import products from './routes/products.js';
 import users from './routes/users.js';
 import orders from './routes/orders.js';
 import uploads from './routes/uploads.js';
+
+// Set __dirname to current directory
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,11 +33,10 @@ app.get('/api/config/paypal', (req, res) =>
 app.use('/api/products', products);
 app.use('/api/users', users);
 app.use('/api/orders', orders);
-app.use('/uploads', uploads);
+app.use('/api/uploads', uploads);
 
-// Set __dirname to current directory
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use(notFound);
+app.use(errorHandler);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ backend/middlewares/errorMiddleware.js
@@ -0,0 +1,25 @@
+// General not found error handler
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  error.status = 404;
+  next(error);
+};
+
+// Custom error middleware
+const errorHandler = (err, req, res, next) => {
+  let statusCode = req.statusCode === 200 ? 500 : req.statusCode;
+  let message = err.message;
+
+  // Mongoose ID validation
+  if (err.name === 'CastError' && err.kind === 'ObjectId') {
+    message = 'Resource not found';
+    statusCode = 404;
+  }
+
+  res.status(statusCode).json({
+    message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
+  });
+};
+
+export { notFound, errorHandler };
