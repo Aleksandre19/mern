3d45f1134d4a1522196e7e21e7c62ee363883fe7
Aleksandre Voronini <aleksandre.voronini@gmail.com>
2024-07-16 02:12:26 +0200

    123. Set uploads directory path for production.

--- backend/main.js
+++ backend/main.js
@@ -65,5 +65,5 @@ if (process.env.NODE_ENV === 'production') {
 // app.use(notFound);
 // app.use(errorHandler);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,8 +1,10 @@
 // export const BASE_URL =
 //   process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '';
+const mode = process.env.NODE_ENV;
 export const BASE_URL = '';
 export const PRODUCTS_URL = '/api/products';
 export const USERS_URL = '/api/users';
 export const ORDERS_URL = '/api/orders';
 export const PAYPAL_URL = '/api/config/paypal';
-export const UPLOADS_URL = '/api/uploads';
+export const UPLOADS_URL =
+  mode === 'development' ? '/api/uploads' : '/var/data/uploads';
